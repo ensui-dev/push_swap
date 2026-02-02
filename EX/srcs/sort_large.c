0a67f395c62d592b1aeede0c30d1a469033df458
Mario Ferreira <marioferreira0212@gmail.com>
2026-02-02 16:00:17 +0000

    progress

--- EX/srcs/sort_large.c
+++ EX/srcs/sort_large.c
@@ -6,10 +6,25 @@
 /*   By: mju-ferr <mju-ferr@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 00:00:00 by ensui             #+#    #+#             */
-/*   Updated: 2026/01/14 10:52:07 by mju-ferr         ###   ########.fr       */
+/*   Updated: 2026/02/02 04:26:04 by mju-ferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-/* TODO: Implement functions for this module */
+void	push_chunks_to_b(t_stack **stack_a, t_stack **stack_b, int chunks)
+{
+	int	size;
+	int	chunk_size;
+	int	chunk;
+	int	min_idx;
+	int	max_idx;
+
+	chunk_size = size / chunks;
+	if (chunk_size == 0)
+		chunk_size = 1;
+	chunk = 0;
+	while (chunk < chunks - 1)
+		min_idx = chunk * chunk_size;
+		max_idx = (chunk + 1) * chunk_size - 1;
+}
--- EX/srcs/sort_small.c
+++ EX/srcs/sort_small.c
@@ -6,7 +6,7 @@
 /*   By: mju-ferr <mju-ferr@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 00:00:00 by ensui             #+#    #+#             */
-/*   Updated: 2026/01/21 21:16:47 by mju-ferr         ###   ########.fr       */
+/*   Updated: 2026/02/02 04:16:56 by mju-ferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,9 @@ void	sort_small(t_stack **stack_a, t_stack **stack_b)
 	else if (size == 3)
 		sort_three(stack_a);
 	else if (size == 4)
-		sort_
+		sort_four(stack_a, stack_b);
+	else if (size == 5)
+		sort_five(stack_a, stack_b);
+	else
+		return ;
 }
